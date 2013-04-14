bc632f214e0fd84076b67c770a8b040e97b13170
Nathanaël Bertrand <cmoinath@gmail.com>
2013-04-14 23:42:31 +0200

    Les affichages de questions sont callé sur les etats reels

--- grails-app/taglib/supersurveys/ConectedTagLib.groovy
+++ grails-app/taglib/supersurveys/ConectedTagLib.groovy
@@ -4,9 +4,9 @@ import org.apache.shiro.SecurityUtils;
 class ConectedTagLib {
 	def logged = { attrs, body ->
 		def u = SecurityUtils.getSubject().getPrincipal()
-
+		
 		if (!u){
-			out << "non connect&eacute; <br/> <a href = \"adrDeLogin\">connection</a>" 
+			out << 'non connect&eacute; <br/> <a href="adrDeLogin">connection</a>'
 		}else{
 			out << "connect&eacute; en tant que " << User.findByUsername(u)
 		}
